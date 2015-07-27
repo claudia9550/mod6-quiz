299261cc48a1defdc64e054d7c43abfcb990b452
claudia9550 <claudia9550@yahoo.com>
2015-07-26 22:40:07 -0700

    Despliegue DB heroku

--- controllers/examen_controller.js
+++ controllers/examen_controller.js
@@ -1,18 +1,22 @@
+var models = require('../models/models.js');
+
 // get question - envia la pregunta
-exports.question=function(req, res) 
-{	 res.render('examen/question', {pregunta: 'Capital de Italia'});
+exports.question = function(req, res) 
+{	models.Quiz.findAll().success(function(quiz) {
+	res.render('examen/question', {pregunta: quiz[0].pregunta});
+    });
 };
 // get answer -da la respuesta
-
 exports.answer=function(req, res) 
 {
- 	if(req.query.respuesta === 'Roma')  {
+    models.Quiz.findAll().success(function(quiz) {
+    if (req.query.respuesta === quiz[0].respuesta)  {
 	res.render('examen/answer', {respuesta: ' Correcto '}); }
 	else {
 	res.render('examen/answer', {respuesta: ' Incorrecto '}); }
+    })
 };
 // datos del autor
-
 exports.creditos=function(req, res)  {	 
         res.render('examen/creditos', {creditos:' CLAUDIA A RODRIGUEZ'}); };
 
